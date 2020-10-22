a17d379238b69dbaad00cfb34bbb7b7f531655d2
Alessandro Passarelli <71466131+Ale1x@users.noreply.github.com>
2020-10-22 11:41:35 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 const isMobileDevice = window.matchMedia('(max-device-width: 820px) and (-webkit-min-device-pixel-ratio: 2)').matches;
 
-const Source = () => <>&nbsp;|&nbsp;<a className='footer-link' href='https://github.com/adamalston/v2'>Source</a></>
+const Source = () => <>&nbsp;|&nbsp;<a className='footer-link' href='https://github.com/ale1x/alessandropassarelli.com'>Codice</a></>
 
 class Footer extends Component {
     render() {
         return (
             <div className='footer-container'>
-                <p className='footer-text' aria-label='Designed and built by Adam Alston'>Designato e CREATO DA&nbsp;
+                <p className='footer-text' aria-label='Designed and built by Adam Alston'>Designato e CREATO DA&nbsp;</p>
                     <a className='footer-link' href='#'>Alessandro Passarelli</a>
                     {isMobileDevice ? '' : <Source />}
                 </p>
